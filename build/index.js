840c79e3a770cb97693940bbd27f18f6b5b85c2e
Yosmany Garcia <yosmanyga@gmail.com>
2021-02-17 12:17:34 -0800

    Added first draft

---
+++ build/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "build", {
+  enumerable: true,
+  get: function get() {
+    return _build.build;
+  }
+});
+
+var _build = require("./build");
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import {build} from './build';
+
+export {
+    build
+};
